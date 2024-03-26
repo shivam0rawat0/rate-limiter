4d89f7c30239799340970501cb1cd4ca113b0377
shivam0rawat0 <shivam100006rawat@gmail.com>
2024-03-26 19:18:45 +0530

    Add missing Qualifier

--- src/main/java/com/sr/RateLimiter/constant/Constant.java
+++ src/main/java/com/sr/RateLimiter/constant/Constant.java
@@ -4,5 +4,5 @@ public class Constant {
     public static final int MAX_REQUESTS_PER_WINDOW = 100;
     public static final int MAX_REQUESTS_PER_IP = 10;
     public static final long IP_PROCESS_WAIT = 500;
-    public static long THREAD_WAIT = 10;
+    public static final long THREAD_WAIT = 10;
 }
